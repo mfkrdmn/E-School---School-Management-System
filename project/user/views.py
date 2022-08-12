ad8a79be9a9b83e6556b953e4950695b2998af09
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-08-12 14:25:36 +0300

    login pages done

--- project/user/views.py
+++ project/user/views.py
@@ -1,5 +1,6 @@
-import re
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,4 +8,20 @@ def login(request):
     return render(request, "login.html")
 
 def teacherlogin(request):
-    return render(request, "login teacher.html")
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'User Invalid')
+            return redirect('/teacherlogin')
+
+
+    else:
+        return render(request, 'login teacher.html')
