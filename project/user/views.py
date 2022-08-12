01614b3741f3ccdc7542a32dd9308f4d68f076cd
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-08-12 13:04:49 +0300

    login register static pages created

--- project/user/views.py
+++ project/user/views.py
@@ -1,3 +1,10 @@
+import re
 from django.shortcuts import render
 
 # Create your views here.
+
+def login(request):
+    return render(request, "login.html")
+
+def teacherlogin(request):
+    return render(request, "login teacher.html")
